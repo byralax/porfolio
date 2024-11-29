50d38fa656ea19c0abca233f23e9e244499756b5
byralax <164099497+byralax@users.noreply.github.com>
2024-11-29 21:10:19 +0100

    Add files via upload

---
+++ PersonalPort.js
@@ -0,0 +1,8 @@
+
+
+var typed = new Typed (".typing",{
+    strings:["Cyber Security Specialist","Web Developer","Passionate F1 fan","Car critic"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
